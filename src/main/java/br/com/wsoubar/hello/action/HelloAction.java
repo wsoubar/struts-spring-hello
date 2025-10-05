5ad2f8b9ff8474411c4f1fdf8b62649f800eae5f
Wagner Barbosa <wsoubar@gmail.com>
2025-10-05 18:04:22 -0300

    logback e ajustes

--- src/main/java/br/com/wsoubar/hello/action/HelloAction.java
+++ src/main/java/br/com/wsoubar/hello/action/HelloAction.java
@@ -1,13 +1,16 @@
 package br.com.wsoubar.hello.action;
 
 import com.opensymphony.xwork2.ActionSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HelloAction extends ActionSupport {
-
+    private static final Logger logger = LoggerFactory.getLogger(HelloAction.class);
     private String message;
 
     @Override
     public String execute() {
+        logger.info("execute()");
         // Mensagem vinda do Spring ou default
         if (message == null) {
             message = "Hello World from Struts 2 + Spring 3!";
