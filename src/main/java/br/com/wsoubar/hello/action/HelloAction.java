d29aa3a421101cd3d29458f96a04b4acba47cfc2
Wagner Barbosa <wsoubar@gmail.com>
2025-10-05 22:22:14 -0300

    ajustado para executar método na action e *.do

--- src/main/java/br/com/wsoubar/hello/action/HelloAction.java
+++ src/main/java/br/com/wsoubar/hello/action/HelloAction.java
@@ -8,9 +8,9 @@ public class HelloAction extends ActionSupport {
     private static final Logger logger = LoggerFactory.getLogger(HelloAction.class);
     private String message;
 
-    @Override
-    public String execute() {
-        logger.info("execute()");
+
+    public String sayHello() {
+        logger.info("sayHello()");
         // Mensagem vinda do Spring ou default
         if (message == null) {
             message = "Hello World from Struts 2 + Spring 3!";
